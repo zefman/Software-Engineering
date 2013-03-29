aabca12c5400c76d20d5adcddaaca822db0d5581
Jozef Maxted <admin@zefwebdesign.co.uk>
2013-03-29 15:10:30 +0000

    Added variables and constructor to Ant class

--- Ant Game Source/Ant.java
+++ Ant Game Source/Ant.java
@@ -1,4 +1,21 @@
 
-public class Ant {
-
+public class Ant {	
+	
+	// All ant vairables
+	private int id;
+	private boolean is_Red;
+	private int direction;
+	private int resting;
+	private boolean has_Food;
+	private int brain_State;
+	
+	public Ant(int id, boolean is_Red) {
+		// Initialise Fields
+		this.id = id;
+		this.is_Red = is_Red;
+		direction = 0;
+		resting = 0;
+		has_Food = false;
+		brain_State = 0;
+	}
 }
