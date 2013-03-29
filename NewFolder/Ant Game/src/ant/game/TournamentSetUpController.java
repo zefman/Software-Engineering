f9c8598d426bd01a6003531cf43da04cf2591ab8
Jozef Maxted <admin@zefwebdesign.co.uk>
2013-03-29 18:25:02 +0000

    Added back to main menu button and function to tournament view and controller

--- NewFolder/Ant Game/src/ant/game/TournamentSetUpController.java
+++ NewFolder/Ant Game/src/ant/game/TournamentSetUpController.java
@@ -4,9 +4,17 @@
  */
 package ant.game;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +23,16 @@ import javafx.fxml.Initializable;
  */
 public class TournamentSetUpController implements Initializable {
 
+    @FXML
+    public void backToMainMenu(ActionEvent event) throws IOException {
+        System.out.println("Back to main menu");
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     /**
      * Initializes the controller class.
      */
