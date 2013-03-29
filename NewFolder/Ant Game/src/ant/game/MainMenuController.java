47d7d985297bca6a42ccbd41442c39e8eaf98971
Jozef Maxted <admin@zefwebdesign.co.uk>
2013-03-29 18:19:17 +0000

    Creating Vvews and controller classes in the JavaFX app

---
+++ NewFolder/Ant Game/src/ant/game/MainMenuController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ant.game;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *s
+ * @author jozefmaxted
+ */
+public class MainMenuController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @FXML
+    private void startTournament(ActionEvent event) throws IOException {
+        System.out.println("Tournament started");
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("TournamentSetUpView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    @FXML
+    private void openBrainEditor(ActionEvent event) {
+        System.out.println("Opening the brain editor");
+    }
+    
+    @FXML
+    private void openWorldEditor(ActionEvent event) {
+        System.out.println("Opening the world editor");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
