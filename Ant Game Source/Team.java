1386563bab8934c291e01decba8d9ed1e01910e7
Jozef Maxted <admin@zefwebdesign.co.uk>
2013-03-29 15:19:04 +0000

    Added variables and constructor to team

--- Ant Game Source/Team.java
+++ Ant Game Source/Team.java
@@ -1,4 +1,19 @@
 
 public class Team {
 
+	private int collectedFood;
+	private int antsRemaining;
+	private int antsStarted;
+	private int matchesWon;
+	private int matchesLost;
+	private int matchesDrawn;
+	private String[] brain;
+	private String teamName;
+	
+	public Team(String teamName, String[] brain) {
+		collectedFood = 0;
+		this.teamName = teamName;
+		this.brain = brain;
+	}
+	
 }
