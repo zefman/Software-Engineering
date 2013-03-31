38faf66f8c6aa3beac2c9cf29a183eda0e55447c
Jozef Maxted <admin@zefwebdesign.co.uk>
2013-03-31 17:10:11 +0100

    Added team class

---
+++ JavaFX/Ant Game/src/ant/game/Team.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ant.game;
+
+/**
+ *
+ * @author jozefmaxted
+ */
+public class Team {
+        private int collectedFood;
+	private int antsRemaining;
+	private int antsStarted;
+	private int matchesWon;
+	private int matchesLost;
+	private int matchesDrawn;
+	private String[] brain;
+	private String teamName;
+	
+	private Team(String teamName) {
+            this.teamName = teamName;
+            collectedFood=0;
+            antsStarted=49;
+            antsRemaining=49;
+	}
+	
+	public void decreaseAnts() {
+            antsRemaining--;
+	}
+	
+	public void increaseCollectedFood() {
+            collectedFood++;
+	}
+	
+	public void updateMatchStats(boolean win, boolean draw) {
+		
+	}
+	
+	public int[] getMatchStats() {
+            int[] matchStats = new int[3];
+            matchStats[0] = matchesWon;
+            matchStats[1] = matchesLost;
+            matchStats[2] = matchesDrawn;
+            return matchStats;
+	}
+	
+	public String getName(){
+            return teamName;
+	}
+	
+	public int getAntsRemaining() {
+            return antsRemaining;
+	}
+	
+	public int getMatchesWon() {
+            return matchesWon;
+	}
+	
+	public int getMatchesLost() {
+            return matchesLost;
+	}
+	
+	public int getMatchesDrawn() {
+            return matchesDrawn;
+	}
+
+	public int getCollectedFood() {
+            return collectedFood;
+	}
+}
