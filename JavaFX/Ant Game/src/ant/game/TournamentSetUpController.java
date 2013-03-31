c37afe197f7203ebe8f326352ca77b610462f3be
Jozef Maxted <admin@zefwebdesign.co.uk>
2013-03-31 00:01:48 +0000

    Added add team functionality to tournament set up

--- JavaFX/Ant Game/src/ant/game/TournamentSetUpController.java
+++ JavaFX/Ant Game/src/ant/game/TournamentSetUpController.java
@@ -7,6 +7,9 @@ package ant.game;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +17,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -22,7 +29,17 @@ import javafx.stage.Stage;
  * @author jozefmaxted
  */
 public class TournamentSetUpController implements Initializable {
-
+    
+    private ObservableList<String> teamNames = FXCollections.observableArrayList("Test", "Team 2");
+    private FadeTransition detailsTransition;
+    private FadeTransition teamsTransition;
+    @FXML
+    private ListView teamListView;
+    @FXML
+    private AnchorPane teamDetailPane;
+    @FXML
+    private TextField teamName;
+    
     @FXML
     public void backToMainMenu(ActionEvent event) throws IOException {
         System.out.println("Back to main menu");
@@ -33,11 +50,33 @@ public class TournamentSetUpController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
+    
+    @FXML
+    public void addTeam(ActionEvent event) {
+        detailsTransition.play();
+        teamsTransition.play();
+        System.out.println(teamName.getText());
+        teamNames.add(teamName.getText());
+    }
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        teamListView.setItems(teamNames);
+        
+        //Set up fade transition
+        detailsTransition = new FadeTransition(Duration.seconds(0.2), teamDetailPane);
+        detailsTransition.setFromValue(1.0);
+        detailsTransition.setToValue(0.0);
+        detailsTransition.setCycleCount(2);
+        detailsTransition.setAutoReverse(true);
+        
+        teamsTransition = new FadeTransition(Duration.seconds(0.2), teamListView);
+        teamsTransition.setFromValue(1.0);
+        teamsTransition.setToValue(0.0);
+        teamsTransition.setCycleCount(2);
+        teamsTransition.setAutoReverse(true);
     }    
 }
