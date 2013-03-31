a4f67d9e6b788cadf724e9f9d756bea31bd74c61
Jozef Maxted <admin@zefwebdesign.co.uk>
2013-03-31 15:37:23 +0100

    Game screen ui finished

--- JavaFX/Ant Game/src/ant/game/GameController.java
+++ JavaFX/Ant Game/src/ant/game/GameController.java
@@ -4,16 +4,23 @@
  */
 package ant.game;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,7 +30,19 @@ import javafx.scene.paint.Color;
 public class GameController implements Initializable {
     
     @FXML
-    BorderPane thePane;
+    AnchorPane canvasPane;
+    
+    
+    @FXML
+    public void backToMainMenu(ActionEvent event) throws IOException {
+        System.out.println("Back to main menu");
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /**
      * Initializes the controller class.
@@ -31,13 +50,13 @@ public class GameController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        Canvas canvas = new Canvas(300,300);
+        Canvas canvas = new Canvas(380,380);
         final GraphicsContext gc = canvas.getGraphicsContext2D();
         
         gc.setFill(Color.BROWN);
         gc.setStroke(Color.BLUE);
         
-        gc.fillRect(0, 0, 300, 300);
+        gc.fillRect(0, 0, 380, 380);
         
         //Draw rocks
         gc.setFill(Color.YELLOW);
@@ -64,6 +83,8 @@ public class GameController implements Initializable {
             }
         });
         
-        thePane.setCenter(canvas);
+        canvasPane.setLeftAnchor(canvas, 15.0);
+        canvasPane.getChildren().add(canvas);
+        
     }    
 }
