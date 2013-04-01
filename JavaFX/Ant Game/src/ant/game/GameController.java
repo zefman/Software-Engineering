6bcb85b03ba6a6686dcb866cb7e41c4015956035
Jozef Maxted <admin@zefwebdesign.co.uk>
2013-04-01 19:40:09 +0100

    Resized game view gui to accomdate bigger canvas

--- JavaFX/Ant Game/src/ant/game/GameController.java
+++ JavaFX/Ant Game/src/ant/game/GameController.java
@@ -103,7 +103,7 @@ public class GameController implements Initializable {
             }
         });
         
-        canvasPane.setLeftAnchor(canvas, 15.0);
+        canvasPane.setLeftAnchor(canvas, 10.0);
         canvasPane.getChildren().add(canvas);
         
     }    
