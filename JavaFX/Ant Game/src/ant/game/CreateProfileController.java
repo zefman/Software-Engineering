1f0315f7f08e24ade5dd7332cdbab16a05f1bd11
Jozef <admin@zefwebdesign.co.uk>
2013-04-02 18:14:21 +0100

    Create profile now creates a temporary file to store the profile while it is worked on

--- JavaFX/Ant Game/src/ant/game/CreateProfileController.java
+++ JavaFX/Ant Game/src/ant/game/CreateProfileController.java
@@ -4,8 +4,17 @@
  */
 package ant.game;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -21,6 +30,8 @@ import javafx.stage.Stage;
 public class CreateProfileController implements Initializable {
     
     private Scene previousScene;
+    private ObservableList<String> brainNames = FXCollections.observableArrayList();
+    private Path tempFile;
 
     @FXML
     public void back(ActionEvent event) {
@@ -28,6 +39,45 @@ public class CreateProfileController implements Initializable {
         Stage stage = (Stage) node.getScene().getWindow();
         stage.setScene(previousScene);
         stage.show();
+    }
+    
+    @FXML
+    /*
+     *
+     */
+    public void addBrain(ActionEvent event) throws IOException {
+        System.out.println("Adding a brain");
+        //Fidn the path of the jar
+        String path = CreateProfileController.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        String decodedPath = URLDecoder.decode(path, "UTF-8");
+        
+        //If the temp file hasn't already been created create one
+        if (tempFile == null) {
+            //Create a temporary file to store the profile while it is being worked on
+            tempFile = Files.createTempFile("tempProfile", ".profile");
+            System.out.println("Temp file " + tempFile);
+            // Delete the temp file once done
+            tempFile.toFile().deleteOnExit();
+        }
+        
+        Charset charset = Charset.forName("US-ASCII");
+        String s = "This is a test";
+
+        try (BufferedWriter writer = Files.newBufferedWriter(tempFile, charset)) {
+            writer.write(s, 0, s.length());
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+
+        try (BufferedReader reader = Files.newBufferedReader(tempFile, charset)) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+        
     }
     
     public void setVariables(Scene previousScene) {
