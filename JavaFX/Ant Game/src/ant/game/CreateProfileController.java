c3845d4f1add828973fab48c72c61a3a44d0b81d
Jozef <admin@zefwebdesign.co.uk>
2013-04-03 00:00:06 +0100

    Adds brains loaded from file to profile temp file

--- JavaFX/Ant Game/src/ant/game/CreateProfileController.java
+++ JavaFX/Ant Game/src/ant/game/CreateProfileController.java
@@ -17,6 +17,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -26,8 +27,10 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -40,11 +43,16 @@ public class CreateProfileController implements Initializable {
     private ObservableList<String> brainNames = FXCollections.observableArrayList();
     private Path tempFile;
     private Path currentBrain;
+    private FadeTransition fadeTransition;
 
     @FXML
     private TextField teamName;
     @FXML
     private Label brainPath;
+    @FXML
+    private Label messageLabel;
+    @FXML
+    private TextField brainName;
     
     @FXML
     public void back(ActionEvent event) {
@@ -59,71 +67,84 @@ public class CreateProfileController implements Initializable {
      *
      */
     public void addBrain(ActionEvent event) throws IOException {
-        System.out.println("Adding a brain");
-        //Fidn the path of the jar
-        String path = CreateProfileController.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        String decodedPath = URLDecoder.decode(path, "UTF-8");
         
-        //If the temp file hasn't already been created create one
-        if (tempFile == null) {
-            //Create a temporary file to store the profile while it is being worked on
-            tempFile = Files.createTempFile("tempProfile", ".profile");
-            System.out.println("Temp file " + tempFile);
-            // Delete the temp file once done
-            tempFile.toFile().deleteOnExit();
-        }
-        
-        Charset charset = Charset.forName("US-ASCII");
-        String s = "This is a test\nThis is a test";
-        
-        //Read the current contents of the file into a List
-        List<String> lines = new ArrayList<String>();
-        /*
-        for (int i = 0; i < lines.size(); i++) {
-            System.out.println("Line: " + i);
-            System.out.println(lines.get(i));
-        }*/
-        
-        //Add the team name to the begining of the string list
-        lines.add(0, "#name# " + teamName.getText());
-        
-        //Put the old file in a list of strings
-        List<String> oldFile = Files.readAllLines(tempFile, charset);
-        
-        //Remove name if it is already there
-        if (!oldFile.isEmpty() && oldFile.get(0).startsWith("#name#")) {
-            oldFile.remove(0);
-        }
-        
-        //Now check to see if the old file contains win stats, if not add them to the new string list
-        if (oldFile.isEmpty() || !oldFile.get(0).startsWith("#wins#")) {
-            lines.add("#wins#");
-            lines.add("#losses#");
-            lines.add("#draws#");
-        }
-        
-        //Add the old file to the new string list
-        lines.addAll(oldFile);
-        
-        //Add new part to string
-        lines.add(s);
+        if (brainName.getText().equals("")) {
+            messageLabel.setText("Please give a brain name.");
+            messageLabel.setTextFill(Color.RED);
+            fadeTransition.play();
+        } else if (currentBrain == null) {
+            messageLabel.setText("Please create or load a brain.");
+            messageLabel.setTextFill(Color.RED);
+            fadeTransition.play();
+        } else {
+            System.out.println("Adding a brain");
+            //Fidn the path of the jar
+            String path = CreateProfileController.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            String decodedPath = URLDecoder.decode(path, "UTF-8");
 
-        try (BufferedWriter writer = Files.newBufferedWriter(tempFile, charset)) {
-            for (String string : lines) {
-                writer.write(string, 0, string.length());
-                writer.newLine();
+            //If the temp file hasn't already been created create one
+            if (tempFile == null) {
+                //Create a temporary file to store the profile while it is being worked on
+                tempFile = Files.createTempFile("tempProfile", ".profile");
+                System.out.println("Temp file " + tempFile);
+                // Delete the temp file once done
+                tempFile.toFile().deleteOnExit();
             }
-        } catch (IOException x) {
-            System.err.format("IOException: %s%n", x);
-        }
 
-        try (BufferedReader reader = Files.newBufferedReader(tempFile, charset)) {
-            String line2 = null;
-            while ((line2 = reader.readLine()) != null) {
-                System.out.println(line2);
+            Charset charset = Charset.forName("US-ASCII");
+
+            //Read the current contents of the file into a List
+            List<String> lines = new ArrayList<String>();
+
+            //Add the team name to the begining of the string list
+            lines.add(0, "#name# " + teamName.getText());
+
+            //Put the old file in a list of strings
+            List<String> oldFile = Files.readAllLines(tempFile, charset);
+
+            //Remove name if it is already there
+            if (!oldFile.isEmpty() && oldFile.get(0).startsWith("#name#")) {
+                oldFile.remove(0);
             }
-        } catch (IOException x) {
-            System.err.format("IOException: %s%n", x);
+
+            //Now check to see if the old file contains win stats, if not add them to the new string list
+            if (oldFile.isEmpty() || !oldFile.get(0).startsWith("#wins#")) {
+                lines.add("#wins#");
+                lines.add("#losses#");
+                lines.add("#draws#");
+            }
+
+            //Add the old file to the new string list
+            lines.addAll(oldFile);
+
+            //Add new brain name after gap
+            lines.add("");
+            lines.add("&" + brainName.getText() + "&");
+            
+            //Add brain to end
+            lines.addAll(Files.readAllLines(currentBrain, charset));
+            
+            //Reset brainName textfield and currentBrain Path
+            brainName.setText("");
+            currentBrain = null;
+
+            try (BufferedWriter writer = Files.newBufferedWriter(tempFile, charset)) {
+                for (String string : lines) {
+                    writer.write(string, 0, string.length());
+                    writer.newLine();
+                }
+            } catch (IOException x) {
+                System.err.format("IOException: %s%n", x);
+            }
+
+            try (BufferedReader reader = Files.newBufferedReader(tempFile, charset)) {
+                String line2 = null;
+                while ((line2 = reader.readLine()) != null) {
+                    System.out.println(line2);
+                }
+            } catch (IOException x) {
+                System.err.format("IOException: %s%n", x);
+            }
         }
         
     }
@@ -158,6 +179,10 @@ public class CreateProfileController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        fadeTransition = new FadeTransition(Duration.seconds(2), messageLabel);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        fadeTransition.setCycleCount(2);
+        fadeTransition.setAutoReverse(true);
     }    
 }
